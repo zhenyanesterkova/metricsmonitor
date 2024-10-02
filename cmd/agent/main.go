0b74440a239f4ef3f33339eb83dc713973506003
EvgeniyaNesterkova <miledieva08@gmail.com>
2024-10-02 18:11:57 +0300

    Клиент - собирает метррики и отправляет на сервер

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,224 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"reflect"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const pollInterval time.Duration = 2 * time.Second
+const reportInterval time.Duration = 10 * time.Second
+
+type metric struct {
+	metricType string
+	value      string
+}
+
+var Metrics = map[string]*metric{
+	"Alloc": &metric{
+		metricType: "gauge",
+	},
+	"BuckHashSys": &metric{
+		metricType: "gauge",
+	},
+	"Frees": &metric{
+		metricType: "gauge",
+	},
+	"GCCPUFraction": &metric{
+		metricType: "gauge",
+	},
+	"GCSys": &metric{
+		metricType: "gauge",
+	},
+	"HeapAlloc": &metric{
+		metricType: "gauge",
+	},
+	"HeapIdle": &metric{
+		metricType: "gauge",
+	},
+	"HeapInuse": &metric{
+		metricType: "gauge",
+	},
+	"HeapObjects": &metric{
+		metricType: "gauge",
+	},
+	"HeapReleased": &metric{
+		metricType: "gauge",
+	},
+	"HeapSys": &metric{
+		metricType: "gauge",
+	},
+	"LastGC": &metric{
+		metricType: "gauge",
+	},
+	"Lookups": &metric{
+		metricType: "gauge",
+	},
+	"MCacheInuse": &metric{
+		metricType: "gauge",
+	},
+	"MCacheSys": &metric{
+		metricType: "gauge",
+	},
+	"MSpanInuse": &metric{
+		metricType: "gauge",
+	},
+	"MSpanSys": &metric{
+		metricType: "gauge",
+	},
+	"Mallocs": &metric{
+		metricType: "gauge",
+	},
+	"NextGC": &metric{
+		metricType: "gauge",
+	},
+	"NumForcedGC": &metric{
+		metricType: "gauge",
+	},
+	"NumGC": &metric{
+		metricType: "gauge",
+	},
+	"OtherSys": &metric{
+		metricType: "gauge",
+	},
+	"PauseTotalNs": &metric{
+		metricType: "gauge",
+	},
+	"StackInuse": &metric{
+		metricType: "gauge",
+	},
+	"StackSys": &metric{
+		metricType: "gauge",
+	},
+	"Sys": &metric{
+		metricType: "gauge",
+	},
+	"TotalAlloc": &metric{
+		metricType: "gauge",
+	},
+	"PollCount": &metric{
+		metricType: "counter",
+		value:      "0",
+	},
+	"RandomValue": &metric{
+		metricType: "gauge",
+	},
+}
+
+func getMetricsStatistic(metrics map[string]*metric, statistic *runtime.MemStats) error {
+
+	runtime.ReadMemStats(statistic)
+	statisticFields := reflect.ValueOf(statistic).Elem()
+
+	for i := 0; i < statisticFields.NumField(); i++ {
+
+		field := statisticFields.Field(i)
+		fieldName := statisticFields.Type().Field(i).Name
+		fieldType := field.Kind()
+		if metric, ok := metrics[fieldName]; ok {
+			switch fieldType {
+			case reflect.Float64:
+				metric.value = strconv.FormatFloat(field.Float(), 'f', -1, 64)
+			case reflect.Uint64:
+				metric.value = strconv.FormatUint(field.Uint(), 10)
+			case reflect.Uint32:
+				metric.value = strconv.FormatUint(field.Uint(), 10)
+			}
+
+			log.Printf("    %s : %s\n", fieldName, metrics[fieldName].value)
+		}
+
+	}
+
+	pollCountValue, err := strconv.ParseInt(metrics["PollCount"].value, 10, 64)
+	if err != nil {
+		return err
+	}
+	metrics["PollCount"].value = strconv.FormatInt(pollCountValue+1, 10)
+	log.Printf("    PollCount : %v\n", metrics["PollCount"].value)
+
+	metrics["RandomValue"].value = strconv.FormatFloat(rand.Float64(), 'f', -1, 64)
+	log.Printf("    RandomValue : %v\n", metrics["RandomValue"].value)
+
+	return nil
+}
+
+func sendQueryUpdateMetric(client *http.Client, mName string, m metric, endpoint string) error {
+
+	builder := strings.Builder{}
+	builder.WriteString(endpoint)
+	builder.WriteString(m.metricType)
+	builder.WriteString("/")
+	builder.WriteString(mName)
+	builder.WriteString("/")
+	builder.WriteString(m.value)
+	url := strings.TrimSpace(builder.String())
+
+	defer builder.Reset()
+
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		log.Printf("can not create request: %v", err)
+		return err
+	}
+
+	req.Header.Set("Content-Type", "text/plain")
+
+	log.Printf("send request to %s\n", url)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("can not do request: %v", err)
+		return err
+	}
+	log.Printf("got response from %s\n", url)
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Printf("can not do request: %v", err)
+		return err
+	}
+	log.Printf("response body:\n")
+	log.Println(string(body))
+
+	return nil
+}
+
+func updateStatistic(n int) error {
+	stats := &runtime.MemStats{}
+	for range n {
+
+		err := getMetricsStatistic(Metrics, stats)
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(pollInterval)
+	}
+	return nil
+}
+
+func main() {
+	endpoint := "http://localhost:8080/update/"
+	client := &http.Client{}
+	for {
+		countUpdateStatistic := int(reportInterval / pollInterval)
+		err := updateStatistic(countUpdateStatistic)
+		if err != nil {
+			panic(err)
+		}
+
+		for name, metric := range Metrics {
+			sendQueryUpdateMetric(client, name, *metric, endpoint)
+		}
+
+	}
+}
