5d5c5ab252096fe8e387a585e29e8ca5f70f83b8
EvgeniyaNesterkova <miledieva08@gmail.com>
2024-10-06 23:26:05 +0300

    add env address; poll_interval; report_interval

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -1,6 +1,10 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+	"strconv"
+)
 
 var (
 	endpoint       string
@@ -15,4 +19,22 @@ func parseFlags() {
 	flag.IntVar(&reportInterval, "r", 10, "the frequency of sending metrics to the server")
 
 	flag.Parse()
+
+	if envEndpoint := os.Getenv("ADDRESS"); envEndpoint != "" {
+		endpoint = envEndpoint
+	}
+	if envPollInterval := os.Getenv("POLL_INTERVAL"); envPollInterval != "" {
+		pollIntervalTemp, err := strconv.Atoi(envPollInterval)
+		if err != nil {
+			panic(err)
+		}
+		pollInterval = pollIntervalTemp
+	}
+	if envReportInterval := os.Getenv("REPORT_INTERVAL"); envReportInterval != "" {
+		envReportIntervalTemp, err := strconv.Atoi(envReportInterval)
+		if err != nil {
+			panic(err)
+		}
+		reportInterval = envReportIntervalTemp
+	}
 }
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
 var endpoint string
@@ -9,4 +10,8 @@ var endpoint string
 func parseFlags() {
 	flag.StringVar(&endpoint, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
+
+	if envEndpoint := os.Getenv("ADDRESS"); envEndpoint != "" {
+		endpoint = envEndpoint
+	}
 }
