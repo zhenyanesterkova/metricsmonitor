c9302f638e64697f09c93fd7a9df6782286e53fe
EvgeniyaNesterkova <miledieva08@gmail.com>
2024-10-08 16:05:11 +0300

    feat: rewrite the structure of the handlers package

--- internal/handlers/handlers.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-type Storage interface {
-	UpdateMetric(name string, typeMetric string, val string) error
-	GetAllMetrics() ([][2]string, error)
-	GetMetricValue(name, metricType string) (string, error)
-}
