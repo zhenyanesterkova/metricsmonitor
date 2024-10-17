125d76e5e043ab444dd3c517e9c29b6605707d46
EvgeniyaNesterkova <miledieva08@gmail.com>
2024-10-17 17:55:29 +0300

    fix(server): del magic strings in metric

--- internal/app/server/metric/metric.go
+++ internal/app/server/metric/metric.go
@@ -6,6 +6,11 @@ import (
 	"github.com/zhenyanesterkova/metricsmonitor/internal/app/server/metric/metricerrors"
 )
 
+const (
+	TypeGauge   = "gauge"
+	TypeCounter = "counter"
+)
+
 type Metric interface {
 	SetValue(string) error
 	GetType() string
@@ -14,9 +19,9 @@ type Metric interface {
 
 func New(typeMetric string) (Metric, error) {
 	switch typeMetric {
-	case "gauge":
+	case TypeGauge:
 		return gauge.NewMetricGauge(), nil
-	case "counter":
+	case TypeCounter:
 		return counter.NewMetricCounter(), nil
 	default:
 		return nil, metricerrors.ErrUnknownType
