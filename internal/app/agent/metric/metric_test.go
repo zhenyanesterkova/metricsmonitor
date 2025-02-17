bfd2f8cbde1d3623a542ada41f54d46923d5adbb
Evgeniya Nesterkova <nester.eva.p@yandex.ru>
2025-02-18 01:01:14 +0300

    fix tests for metric

--- internal/app/agent/metric/metric_test.go
+++ internal/app/agent/metric/metric_test.go
@@ -1,6 +1,7 @@
 package metric
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -38,4 +39,57 @@ func Test_updateCounter(t *testing.T) {
 		metrica.updateCounter()
 		assert.Equal(t, expectMetrica, metrica)
 	})
+}
+
+func Test_UpdateMetrics(t *testing.T) {
+	var metricsTest = NewMetricBuf()
+	type args struct {
+		metrics    *MetricBuf
+		statStruct *runtime.MemStats
+	}
+	tests := []struct {
+		args    args
+		name    string
+		wantErr bool
+	}{
+		{
+			name: "test #1 correct update metrics",
+			args: args{
+				metrics:    metricsTest,
+				statStruct: &runtime.MemStats{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.args.metrics.UpdateMetrics()
+		})
+	}
+}
+
+func Test_UpdateGopsutilMetrics(t *testing.T) {
+	var metricsTest = NewMetricBuf()
+	type args struct {
+		metrics    *MetricBuf
+		statStruct *runtime.MemStats
+	}
+	tests := []struct {
+		args    args
+		name    string
+		wantErr bool
+	}{
+		{
+			name: "test #1 correct update metrics",
+			args: args{
+				metrics:    metricsTest,
+				statStruct: &runtime.MemStats{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.args.metrics.UpdateGopsutilMetrics()
+			assert.NoError(t, err)
+		})
+	}
 }
